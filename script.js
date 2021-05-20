232fc9986ae825fd44c8c78da5a32ae3656e6b42
Loryn <lorynhaykus@yahoo.com>
2021-05-19 21:46:00 -0600

    got some colors to show up

--- script.js
+++ script.js
@@ -1,8 +1,46 @@
 $(document).ready(function () {
-let today;
 
 //Displaying the current day using moment.js
-today = moment().format("MMM Do, YYYY");
+let today = moment().format("MMM Do, YYYY");
 $("#currentDay").append(today);
 
+//Variable for current hour
+let currentHour = moment().format("H");
+
+const eventTime = currentHour;
+
+
+
+//Function to change the color of the text box depending on what time it is
+$("textarea").each(function() {
+  
+   // let eventTime = $(this).attr("textarea")
+    //If the current hour is the less than the hour for the event on the calender the background is changed to the class "past"
+        if(currentHour > eventTime) {
+            $(this).addClass("past");
+    //If current hour is the same as event hour then the background class is "present"
+        }else if(currentHour === eventTime) {
+            $(this).addClass("present");
+    //If current hour is greater than the event hour than the background class is "future"
+        }else if(currentHour < eventTime) {
+            $(this).addClass("future");
+      
+      
+        }
+    })
+
+
+
+
+//Saved to local storage
+
+
+
+
+
+//localStorage.text
+//document.getElementById("textarea").innerHTML = localStorage.text;
+
+
 })
+
