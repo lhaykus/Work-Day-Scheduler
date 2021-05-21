beeff7baba612758a9bdb5670f4d2ef14cd5fd6c
Loryn <lorynhaykus@yahoo.com>
2021-05-21 16:33:33 -0600

    working out the local storage

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ let today = moment().format("MMM Do, YYYY");
 $("#currentDay").append(today);
 
 //Variable for current hour
-let currentHour = moment().format("h:mm:ss a");
-$("#currentHour").append(currentHour);
+let currentHour = moment().hours();
+
 let eventTime = document.getElementsByClassName("hour");
 
 //Targeting the class "textarea" and creating a function for each element of that id
@@ -19,10 +19,10 @@ $(".textarea").each(function() {
         if(currentHour > eventTime) {
             $(this).addClass("past");
     //If current hour is the same as event hour then the background class is "present"
-        }else if(currentHour === eventTime) {
+        } else if(currentHour === eventTime) {
             $(this).addClass("present");
     //If current hour is greater than the event hour than the background class is "future"
-        }else if(currentHour < eventTime) {
+        } else if(currentHour < eventTime) {
             $(this).addClass("future");
       
       
@@ -33,13 +33,28 @@ $(".textarea").each(function() {
 
 
 //Saved to local storage
+$(".saveBtn").on("click", function(){
+    const text = document.getElementsByClassName("textarea");
+    localStorage.setItem('textarea', text.value);
+   
+    const time = document.getElementsByClassName("hour");
+    localStorage.setItem("hour",time.value);
+  // const text = $(this).siblings("textarea").val();
+  // const time = $(this).siblings("textarea").attr("id");
+   console.log(this);
 
+    localStorage.setItem(text, time);
+});
 
-
-
-
-//localStorage.text
-//document.getElementById("textarea").innerHTML = localStorage.text;
+$("#9 .textarea").val(localStorage.getItem("9"));
+$("#10 .description").val(localStorage.getItem("10"));
+$("#11 .description").val(localStorage.getItem("11"));
+$("#12 .description").val(localStorage.getItem("12"));
+$("#13 .description").val(localStorage.getItem("13"));
+$("#14 .description").val(localStorage.getItem("14"));
+$("#15 .description").val(localStorage.getItem("15"));
+$("#16 .description").val(localStorage.getItem("16"));
+$("#17 .description").val(localStorage.getItem("17"));
 
 
 })
